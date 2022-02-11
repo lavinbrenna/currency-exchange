061a7bfa24d062e9bbe246dd17a22eb076630646
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-11 08:30:44 -0800

    add currency conversion js, update request url

---
+++ src/currency.js
@@ -0,0 +1,18 @@
+export default class CurrencyConversion {
+  static async getRate(query) {
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${query}`
+    )
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return Error(error);
+      });
+  }
+}
+
+//currency is divided not multiplied
