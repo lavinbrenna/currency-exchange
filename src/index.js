b624ce337a05008e2d9973bf9ef08a916578fa97
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-11 12:36:14 -0800

    update UI functionality

--- src/index.js
+++ src/index.js
@@ -1,54 +1,74 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import CurrencyConversion from './currency.js';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import CurrencyConversion from "./currency.js";
 
-function clearFields(){
-  $('#userDollars').val("");
-  $('#userCode').val("");
-  $('.showErrors').text("");
+function clearFields() {
+  $("#userDollars").val("");
+  $("#userCode").val("");
+  $(".showErrors").text("");
 }
 
-function displayExchangeRate(q,rate){
+function displayExchangeRate(q, rate) {
   $(".exchangeOutput").text(`The exchange rate from USD to ${q} is ${rate}.`);
 }
 
-function displayErrors(error){
-  $('.showErrors').text(`${error}`);
+function displayErrors(error) {
+  $(".showErrors").text(`${error}`);
 }
 
-function displayConvertedMoney(money,q,rate){
+function displayConvertedMoney(money, q, rate) {
   let convertedAmount = parseFloat(money) / rate;
-  $('.convertedMoney').text(`$${money} converted to ${q} is equal to ${convertedAmount.toFixed(2)} ${q}`);
+  $(".convertedMoney").text(
+    `$${money} converted to ${q} is equal to ${convertedAmount.toFixed(2)} ${q}`
+  );
 }
 
-$(document).ready(function(){
-  $('#convertMe').on('click', function(){
-    let query = $('input#userCode').val();
-    let userMoney = $('input#userDollars').val();
-    if((query === "") && (userMoney=== "")){
-      $('.showErrors').text('Please enter a number and three letter currency code!');
-    }else if(query ===""){
-      $(".showErrors").text('Please enter a three letter currency code');
-    }
-    else if(userMoney === ""){
-      $(".showErrors").text('Please enter a valid dollar amount');
-    }
-    else{
+$(document).ready(function () {
+  $("#convertMe, #limitedConvert").on("click", function () {
+    let query = $("input#userCode").val();
+    let userMoney = $("input#userDollars").val();
+    let limitedQuery = $("select#currencyCode option:selected").val();
+    let usDollars = $("input#usDollars").val();
+    if (query != "" && userMoney != "") {
       clearFields();
       CurrencyConversion.getRate(query)
-        .then(function (currencyResponse){
-          if(currencyResponse instanceof Error){
-            throw Error(`I'm sorry there was an ExchangeRate-API ${currencyResponse}`);
+        .then(function (currencyResponse) {
+          if (currencyResponse instanceof Error) {
+            throw Error(
+              `I'm sorry there was an ExchangeRate-API ${currencyResponse}`
+            );
           }
           const rate = currencyResponse.conversion_rate;
           displayExchangeRate(query, rate);
           displayConvertedMoney(userMoney, query, rate);
         })
-        .catch(function (error){
+        .catch(function (error) {
           displayErrors(error);
         });
+    } else if (limitedQuery != "default" && usDollars != ""){
+      clearFields();
+      CurrencyConversion.getRate(limitedQuery)
+        .then(function (currencyResponse) {
+          if (currencyResponse instanceof Error) {
+            throw Error(
+              `I'm sorry there was an ExchangeRate-API ${currencyResponse}`
+            );
+          }
+          const rate = currencyResponse.conversion_rate;
+          displayExchangeRate(limitedQuery, rate);
+          displayConvertedMoney(usDollars, limitedQuery, rate);
+        })
+        .catch(function (error) {
+          displayErrors(error);
+        });
+    } else if (userMoney === "" || usDollars === "") {
+      $(".showErrors").text("Please enter a valid dollar amount");
+    } else if (query === "" || limitedQuery === "default") {
+      $(".showErrors").text("Please select a currency code");
+    } else {
+      clearFields();
     }
   });
 });
